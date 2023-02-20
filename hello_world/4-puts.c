d18f8a50c0d694044fea7ac27d1e95e90fcc8ac9
Nilesoj20 <jvrn2014@gmail.com>
2023-02-20 06:32:21 -0800

    fix: task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* imprimir un texto con salto de linea */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
