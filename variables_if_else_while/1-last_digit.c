b4cbaedce8f431640a2386fddad8fa60f2f2fe18
Nilesoj20 <jvrn2014@gmail.com>
2023-02-21 14:52:08 -0800

    fix: task 1  assign a random number to the variable n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 
 	if (m > 5)
 	{
-		printf("last digit of %d is greater than %d: and is greater than 5\n", n, m);
+		printf("last digit of %d is %d and is greater than 5\n", n, m);
 	}
 	else if (m == 0)
 	{
-		printf("last digit of %d is %d: the string and is 0\n", n, m);
+		printf("last digit of %d is %d and is 0\n", n, m);
 	}
 	else
 	{
