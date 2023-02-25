0c58fafe303f9fe6fb93f8ee52757a5ccd77d3bd
Nilesoj20 <jvrn2014@gmail.com>
2023-02-24 20:29:30 -0800

    feat: task 10 adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - Entry point
+* @a: input number
+* @b : input number
+* Return: Always one resultado
+*/
+int add(int a, int b)
+{
+	int resultado;
+
+	resultado = a + b;
+
+	return (resultado);
+}
