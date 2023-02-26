eb52fa8d9950aaf6071b0a3f473d2ade20c2a057
Nilesoj20 <jvrn2014@gmail.com>
2023-02-26 15:26:18 -0800

    feat: task 10 prints a triangle, followed by a new line

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* print_triangle - Entry point
+* @size: The number to be checked
+* Return: Always 0 (Success)
+*/
+void print_triangle(int size)
+{
+	int l, p, m, sizep, x;
+
+	l = 0;
+	p = 0;
+	m = 0;
+	sizep = size - 1;
+	x = 1;
+
+	while (l < size)
+	{
+		while (p < sizep)
+		{
+			_putchar(' ');
+			p++;
+		}
+		sizep--;
+		p = 0;
+		while (m < x)
+		{
+			_putchar(35);
+			m++;
+		}
+		m = 0;
+		x++;
+		l++;
+		_putchar('\n');
+	}
+}
