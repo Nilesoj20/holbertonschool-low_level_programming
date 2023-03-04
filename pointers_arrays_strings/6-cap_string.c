57bfcb97ac353504d9777fce95c2f7062b928f4c
Nilesoj20 <jvrn2014@gmail.com>
2023-03-03 21:25:41 -0800

    feat: task 6 Write a function that capitalizes all words of a strin

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - check the code
+ * @str: pointer
+ * Return: Always 0.
+ */
+char *cap_string(char *str)
+{
+	char *ptemp;
+	int alerta;
+
+	ptemp = str;
+	alerta = 0;
+
+	while (*ptemp != '\0')
+	{
+		if (*ptemp == ' ' || *ptemp == '\t' || *ptemp == '\n' ||
+				*ptemp == ',' || *ptemp == 59 || *ptemp == 46 ||
+				*ptemp == 33 || *ptemp == 63 || *ptemp == 34 ||
+				*ptemp == 40 || *ptemp == 41 || *ptemp == 123 ||
+				*ptemp == 125)
+		{
+			alerta = 1;
+		}
+		else
+		{
+			if (alerta == 1)
+			{
+				*ptemp = *ptemp - 32;
+				alerta = 0;
+			}
+		}
+		ptemp++;
+	}
+	return (str);
+}
