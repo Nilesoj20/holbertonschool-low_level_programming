6106fb13d4479e5308040d2bd6852502358afdc4
Nilesoj20 <jvrn2014@gmail.com>
2023-03-09 21:08:34 -0800

    fix task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 /**
-* main - funcion princila
+* main - funcion principal
 * @argc: name program
 * @argv: array
 * Return: Always 0.
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute ((unused)))
 {
+	if (argc == 0)
+	{
+		printf("0");
+	}
+	else
+	{
 	printf("%d\n", argc - 1);
+	}
 	return (0);
 }
