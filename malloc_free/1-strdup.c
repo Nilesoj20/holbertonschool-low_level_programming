8683350f064b3c42e923b03a8e65ef675f0ba068
Nilesoj20 <jvrn2014@gmail.com>
2023-03-14 20:48:29 -0700

    feat: task 1 nction that returns a pointer to a newly allocated space in memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strlen - check
+* @s: pointer
+* Return: variable cont.
+*/
+int _strlen(char *s)
+{
+	int cont;
+
+	for (cont = 0; *s != '\0'; cont++)
+	{
+		s++;
+	}
+	return (cont);
+}
+/**
+* _strdup - check the code
+* @str: pointer
+* Return: pointer ptem.
+*/
+char *_strdup(char *str)
+{
+	size_t i;
+	size_t len = _strlen(str) + 1;
+	char *ptem = malloc(len * sizeof(char));
+
+	if (str == NULL || ptem == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		ptem[i] = str[i];
+	}
+	return (ptem);
+}
