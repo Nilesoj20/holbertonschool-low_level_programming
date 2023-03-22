7f7f04a85c7392c98ad05ea076e11c707c163785
Nilesoj20 <jvrn2014@gmail.com>
2023-03-21 21:50:48 -0700

    feat task 5 function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - check the code
+ * @d: pointer
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	free(ptr->name);
+	free(ptr->owner);
+}
