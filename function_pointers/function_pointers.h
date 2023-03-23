eec03e4462261ffc3da2c8d47e0baf45a1a51797
Nilesoj20 <jvrn2014@gmail.com>
2023-03-23 14:31:04 -0700

    feat: task 1function that executes a function given as a parameter on each element of an array

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
+#include <stddef.h>
 #ifndef __FUNCTION_POINTERS_H__
 #define __FUNCTION_POINTERS_H__
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /*DOG.H*/
