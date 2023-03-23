f977d9f270194a8fdf8b52d6e80d70d295acc16b
Nilesoj20 <jvrn2014@gmail.com>
2023-03-22 20:19:14 -0700

    fix: task 0 NULL validation

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+ #include <stdlib.h>
 /**
  * print_name - prints a name
  * @name: name of the person
@@ -9,6 +10,10 @@ void print_name(char *name, void (*f)(char *))
 {
 	void (*p)(char *);
 
+	if (f == NULL)
+	{
+		return;
+	}
 	p = f;
 	p(name);
 }
