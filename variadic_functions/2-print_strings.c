3e6be6dfe1c9c5154099f0998c59c06e34abe96d
Nilesoj20 <jvrn2014@gmail.com>
2023-03-25 16:25:51 -0700

    feat: task 2 function that prints strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - check the code
+ * @separator: separador
+ * @n: tamaño
+ * Return: numeros.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	unsigned int i;
+	char *nil;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	va_start(str, n);
+	for (i = 0; i < n; i++)
+	{
+		nil = va_arg(str, char*);
+		if (nil == NULL)
+		{
+			nil = "(nil)";
+		}
+		printf("%s", nil);
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(str);
+}
