56e8e48ec0cdc27b964aec846041ac68c5c370a7
Nilesoj20 <jvrn2014@gmail.com>
2023-03-25 15:44:13 -0700

    feat: task 1 function that prints numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - check the code
+ * @separator: separador
+ * @n: tamaño
+ * Return: numeros.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num, int));
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(num);
+}
