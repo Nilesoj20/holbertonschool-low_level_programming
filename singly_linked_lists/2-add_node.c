db8f9f70f1a47f0687f22b9e6d4480db306aabd4
Nilesoj20 <jvrn2014@gmail.com>
2023-04-07 13:43:43 -0700

    fix: checking when str receives null

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,11 +12,16 @@ list_t *add_node(list_t **head, const char *str)
 	tmp = malloc(sizeof(list_t));
 	if (tmp == NULL)
 	{
-		exit(1);
+		return (NULL);
 	}
 	tmp->str = strdup(str);
+	if (tmp->str == NULL)
+	{
+		free(tmp);
+		return (NULL);
+	}
 	tmp->len = strlen(str);
 	tmp->next = *head;
 	*head = tmp;
-	return (0);
+	return (tmp);
 }
