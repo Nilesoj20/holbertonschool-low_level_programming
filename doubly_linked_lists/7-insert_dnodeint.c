2ed4a413ed5f18fc90c46a06bc2e7cb299a8f2e6
Nilesoj20 <jvrn2014@gmail.com>
2023-04-10 13:41:27 -0700

    fix: task 7 number items

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -42,7 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		pos = pos->next;
 	}
-	if (pos->next == NULL)
+	if (pos->next == NULL && idx == size(*h))
 		return (add_dnodeint_end(h, n));
 	tmp = malloc(sizeof(dlistint_t));
 	if (tmp == NULL)
