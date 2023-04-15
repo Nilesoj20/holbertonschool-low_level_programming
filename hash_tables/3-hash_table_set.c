ac35ad5ddc368a77e7796c469ce1db1cba1c6826
Nilesoj20 <jvrn2014@gmail.com>
2023-04-15 14:29:16 -0700

    feat: function that adds an element to the hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - agrega un elemento a la tabla hash
+ * @ht: tabla ya creada
+ * @key: llave
+ * @value: valor asignar
+ * Return: 1 exito, 0 falla
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *nodo;
+	hash_node_t *tmp;
+
+	if (!ht || !key || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL)
+	{
+		tmp = ht->array[index];
+		while (strcmp(tmp->key, key) != 0)
+		{
+			tmp = tmp->next;
+		}
+			free(ht->array[index]->value);
+			ht->array[index]->value = strdup(value);
+			return (1);
+	}
+	if (ht->array[index] == NULL)
+	{
+		nodo = malloc(sizeof(hash_node_t));
+		if (!nodo)
+			return (0);
+		nodo->key = strdup(key);
+		nodo->value = strdup(value);
+		nodo->next = ht->array[index];
+		ht->array[index] = nodo;
+	}
+	return (1);
+}
